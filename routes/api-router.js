46efa42f4e9d17c30e439e182c088e093907c3b6
Janilee <janileesvardstal@gmail.com>
2024-11-29 16:50:39 +0000

    refactors project to include routers for all endpoints

---
+++ routes/api-router.js
@@ -0,0 +1,17 @@
+const endpoints = require("../endpoints.json");
+const apiRouter = require('express').Router();
+const articlesRouter = require('./articles-router');
+const commentsRouter = require('./comments-router');
+const topicsRouter = require('./topics-router');
+const usersRouter = require('./users-router');
+
+apiRouter.get("/", (req, res) => {
+  res.status(200).send({ endpoints })
+})
+
+apiRouter.use("/articles", articlesRouter);
+apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/topics", topicsRouter);
+apiRouter.use("/users", usersRouter);
+
+module.exports = apiRouter;
