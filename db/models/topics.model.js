83b76e0f66369954ffc9a6fa6ba75f26be44e069
Janilee <janileesvardstal@gmail.com>
2024-12-07 00:42:32 +0000

    adds post-functionality to the api/topics endpoint and tests

--- db/models/topics.model.js
+++ db/models/topics.model.js
@@ -7,3 +7,25 @@ exports.selectAllTopics = () => {
       return rows;
     })
 };
+
+exports.insertTopic = (topic, description) => {
+  return db.query(`SELECT * FROM topics WHERE slug = $1;`, [topic])
+  .then(({ rows }) => {
+
+    if (rows.length > 0) {
+      return Promise.reject({ status: 409, msg: "Topic already exists" })
+
+    } else {
+      const sqlQuery = `
+        INSERT INTO topics (slug, description)
+        VALUES ($1, $2)
+        RETURNING slug, description;
+      `;
+
+      return db.query(sqlQuery, [topic, description])
+        .then(({ rows }) => {
+          return rows[0];
+      })
+    }
+  })
+}
